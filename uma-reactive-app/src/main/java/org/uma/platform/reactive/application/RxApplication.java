49db09f632bca38fd500046fb5a053d0ef9981bb
mk10969 <mk10969@gmail.com>
2019-11-17 03:05:39 +0900

    spring multi project

---
+++ uma-reactive-app/src/main/java/org/uma/platform/reactive/application/RxApplication.java
@@ -0,0 +1,13 @@
+package org.uma.platform.reactive.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxApplication.class, args);
+
+    }
+}
---
+++ uma-reactive-app/src/test/java/org/uma/platform/reactive/application/RxApplicationTest.java
@@ -0,0 +1,9 @@
+package org.uma.platform.reactive.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RxApplicationTest {
+
+
+
+}
