6b964ce6eb7cd7205048228d12848fd7ac19f3fa
mk10969 <mk10969@gmail.com>
2019-11-26 18:52:41 +0900

    record 設定の追加

--- uma-feed-app/src/main/java/org/uma/platform/feed/application/configuration/JvLinkRecordSpecConfiguration.java
+++ uma-feed-app/src/main/java/org/uma/platform/feed/application/configuration/JvLinkRecordSpecConfiguration.java
@@ -20,7 +20,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return レース詳細
      */
     @Bean(name = "RA")
-    @ConfigurationProperties(prefix = "ra")
+    @ConfigurationProperties(prefix = "RA")
     RecordSpecItems configRA() {
         return new RecordSpecItems();
     }
@@ -29,7 +29,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 出走馬詳細
      */
     @Bean(name = "SE")
-    @ConfigurationProperties(prefix = "se")
+    @ConfigurationProperties(prefix = "SE")
     RecordSpecItems configSE() {
         return new RecordSpecItems();
     }
@@ -38,17 +38,26 @@ public class JvLinkRecordSpecConfiguration {
      * @return レース払戻
      */
     @Bean(name = "HR")
-    @ConfigurationProperties(prefix = "hr")
+    @ConfigurationProperties(prefix = "HR")
     RecordSpecItems configHR() {
         return new RecordSpecItems();
     }
 
     /**
-     * @return レース票数
+     * @return レース票数（単勝・複勝・枠連・馬連・ワイド・馬単・三連複）
      */
     @Bean(name = "H1")
-    @ConfigurationProperties(prefix = "h1")
+    @ConfigurationProperties(prefix = "H1")
     RecordSpecItems configH1() {
+        return new RecordSpecItems();
+    }
+
+    /**
+     * @return 競走馬除外情報
+     */
+    @Bean(name = "JG")
+    @ConfigurationProperties(prefix = "JG")
+    RecordSpecItems configJG() {
         return new RecordSpecItems();
     }
 
@@ -56,7 +65,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 産駒マスタ
      */
     @Bean(name = "SK")
-    @ConfigurationProperties(prefix = "sk")
+    @ConfigurationProperties(prefix = "SK")
     RecordSpecItems configSK() {
         return new RecordSpecItems();
     }
@@ -65,7 +74,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 系統情報
      */
     @Bean(name = "BT")
-    @ConfigurationProperties(prefix = "bt")
+    @ConfigurationProperties(prefix = "BT")
     RecordSpecItems configBT() {
         return new RecordSpecItems();
     }
@@ -74,7 +83,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 繁殖馬マスタ
      */
     @Bean(name = "HN")
-    @ConfigurationProperties(prefix = "hn")
+    @ConfigurationProperties(prefix = "HN")
     RecordSpecItems configHN() {
         return new RecordSpecItems();
     }
@@ -83,7 +92,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return コース情報
      */
     @Bean(name = "CS")
-    @ConfigurationProperties(prefix = "cs")
+    @ConfigurationProperties(prefix = "CS")
     RecordSpecItems configCS() {
         return new RecordSpecItems();
     }
@@ -93,7 +102,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 競走馬マスタ
      */
     @Bean(name = "UM")
-    @ConfigurationProperties(prefix = "um")
+    @ConfigurationProperties(prefix = "UM")
     RecordSpecItems configUM() {
         return new RecordSpecItems();
     }
@@ -102,7 +111,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 騎手マスタ
      */
     @Bean(name = "KS")
-    @ConfigurationProperties(prefix = "ks")
+    @ConfigurationProperties(prefix = "KS")
     RecordSpecItems configKS() {
         return new RecordSpecItems();
     }
@@ -111,7 +120,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 調教師マスタ
      */
     @Bean(name = "CH")
-    @ConfigurationProperties(prefix = "ch")
+    @ConfigurationProperties(prefix = "CH")
     RecordSpecItems configCH() {
         return new RecordSpecItems();
     }
@@ -120,7 +129,7 @@ public class JvLinkRecordSpecConfiguration {
      * @return 生産者マスタ
      */
     @Bean(name = "BR")
-    @ConfigurationProperties(prefix = "br")
+    @ConfigurationProperties(prefix = "BR")
     RecordSpecItems configBR() {
         return new RecordSpecItems();
     }
@@ -129,8 +138,56 @@ public class JvLinkRecordSpecConfiguration {
      * @return 馬主マスタ
      */
     @Bean(name = "BN")
-    @ConfigurationProperties(prefix = "bn")
+    @ConfigurationProperties(prefix = "BN")
     RecordSpecItems configBN() {
+        return new RecordSpecItems();
+    }
+
+
+    ///////// リアルタイム //////////
+
+    /**
+     * @return 馬体重情報
+     */
+    @Bean(name = "WH")
+    @ConfigurationProperties(prefix = "WH")
+    RecordSpecItems configWH() {
+        return new RecordSpecItems();
+    }
+
+    /**
+     * @return 天候馬場状態
+     */
+    @Bean(name = "WE")
+    @ConfigurationProperties(prefix = "WE")
+    RecordSpecItems configWE() {
+        return new RecordSpecItems();
+    }
+
+    /**
+     * @return 出走取消競争除外
+     */
+    @Bean(name = "AV")
+    @ConfigurationProperties(prefix = "AV")
+    RecordSpecItems configAV() {
+        return new RecordSpecItems();
+    }
+
+    /**
+     * @return 騎手変更
+     */
+    @Bean(name = "JC")
+    @ConfigurationProperties(prefix = "JC")
+    RecordSpecItems configJC() {
+        return new RecordSpecItems();
+    }
+
+    /**
+     * @return 発走時刻変更
+     */
+    @Bean(name = "TC")
+    @ConfigurationProperties(prefix = "TC")
+    RecordSpecItems configTC() {
         return new RecordSpecItems();
     }
 
