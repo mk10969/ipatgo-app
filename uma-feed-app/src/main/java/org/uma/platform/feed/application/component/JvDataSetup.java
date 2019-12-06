fd7a229a9d355e878cdea678af2925976b6a1521
mk10969 <mk10969@gmail.com>
2019-12-06 21:12:34 +0900

    service化できるようになった。

--- uma-feed-app/src/main/java/org/uma/platform/feed/application/component/JvDataSetup.java
+++ uma-feed-app/src/main/java/org/uma/platform/feed/application/component/JvDataSetup.java
@@ -1,47 +1,45 @@
-//package org.uma.platform.feed.application.setup;
-//
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-//import org.springframework.stereotype.Component;
-//import org.uma.platform.common.config.Option;
-//import org.uma.platform.feed.application.repository.JvLinkStoredRepository;
-//import reactor.core.publisher.Flux;
-//import reactor.core.scheduler.Schedulers;
-//
-//import javax.annotation.PostConstruct;
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Component
-//@RequiredArgsConstructor
-//@Profile("setup")
-//public class JvDataSetup {
-//
-//    @Value("${data.yearsOld}")
-//    private long yearsOld;
-//
-//    private final List<JvLinkStoredRepository> jvLinkStoredRepositories;
-//
-//    private final ReactiveMongoTemplate template;
-//
-//
-////    @PostConstruct
-////    public void setup() {
-////        LocalDateTime dateTime = LocalDateTime.now()
-////                .minusYears(yearsOld);
-////
-////        Flux.fromIterable(jvLinkStoredRepositories)
-////                .publishOn(Schedulers.single())  // 訳ありシングルスレッド
-////                .flatMap(repository -> repository
-////                        .fetch(dateTime, Option.SETUP_WITHOUT_DIALOG)
-////
-////                )
-////
-////
-////    }
-//
-//
-//}
+package org.uma.platform.feed.application.component;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.stereotype.Component;
+import org.uma.platform.common.config.Option;
+import org.uma.platform.feed.application.repository.JvLinkStoredRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Profile("setup")
+public class JvDataSetup {
+
+    @Value("${data.yearsOld}")
+    private long yearsOld;
+
+    @Value("{data.filters}")
+    private List<String> fliters;
+
+    private final List<JvLinkStoredRepository> jvLinkStoredRepositories;
+
+    private final ReactiveMongoTemplate template;
+
+    @PostConstruct
+    public void setup() {
+        LocalDateTime dateTime = LocalDateTime.now()
+                .minusYears(yearsOld);
+
+//        Flux.fromIterable(jvLinkStoredRepositories)
+//                .publishOn(Schedulers.single())  // 訳ありシングルスレッド
+//                .flatMap(repository -> repository.readFlux(dateTime, Option.SETUP_WITHOUT_DIALOG)
+//                        .flatMap(i ->));
+    }
+
+
+}
--- uma-feed-app/src/test/java/org/uma/platform/feed/application/test/ReactorTest.java
+++ uma-feed-app/src/test/java/org/uma/platform/feed/application/test/ReactorTest.java
@@ -1,13 +1,28 @@
 package org.uma.platform.feed.application.test;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ReactorTest {
+
+    @Test
+    void test_allメソッドテスト() throws InterruptedException {
+        Flux.just("hoge", "foo", "bar")
+                .all(Objects::nonNull) // 中身全てチェックするということね。
+                .doOnNext(System.out::println)
+                .subscribe();
+        Thread.sleep(1000L);
+    }
+
 
     @Test
     void test_まず初めに() {
@@ -39,20 +54,30 @@ public class ReactorTest {
                 .verifyComplete();
     }
 
+    private Flux<String> flux = Flux.just("aaa,bb", "b,ccc,ddddd,e", "ee,ff", "ff,g,hh,i", "i", "iii,jjj,")
+            .repeat()
+            .bufferUntil(i -> i.endsWith(","))
+            .flatMap(i -> Flux.fromArray(i.stream().collect(Collectors.joining()).split(",")))
+            .doOnNext(System.out::println);
+
     @Test
-    void pass3() {
-        Flux.just("aaa,bb", "b,ccc,ddddd,e", "ee,ff", "ff,g,hh,i", "i", "iii,jjj,")
-                .repeat()
-//                .flatMap(i -> Flux.from())
-//
-//                .reduce((i, j) -> i + j)
-                .doOnNext(System.out::println)
-//                .flatMapMany(i -> Flux.fromArray(i.split(",")))
-//                .doOnNext(System.out::println)
-//                .take(3)
-                .subscribe();
+    void test_カンマスピリットwithリピート_and_verifyComplete() {
+        StepVerifier.create(flux)
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .verifyComplete();
     }
 
+    @Test
+    void test_カンマスピリットwithリピート_and_verify() {
+        StepVerifier.create(flux)
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .expectNext("aaa", "bbb", "ccc", "ddddd", "eee", "ffff", "g", "hh", "iiiii", "jjj")
+                .expectComplete() //完了を期待するので、エラーになる。
+                .verify();
+    }
 
     @Test
     void testtest() {
@@ -62,5 +87,33 @@ public class ReactorTest {
         System.out.println(arr.length);
     }
 
+    @Test
+    void test_HotFlux() {
+        // これ動きが面白いね。
+        DirectProcessor<String> hotSource = DirectProcessor.create();
+        Flux<String> hotFlux = hotSource.map(String::toUpperCase);
+
+        hotFlux.subscribe(d -> System.out.println("No1: " + d));
+        hotSource.onNext("blue");
+        hotSource.onNext("green");
+
+        hotFlux.subscribe(d -> System.out.println("No2: " + d));
+        hotSource.onNext("orange");
+        hotSource.onNext("purple");
+
+        hotSource.onComplete(); //終わりの合図。
+    }
+
+    private Function<Flux<String>, Flux<String>> fillterAndMap = f -> f.filter(i -> !i.equals("b")).map(String::toUpperCase);
+
+    @Test
+    void test_transform() {
+        Flux<String> transformFlux = Flux.fromIterable(Arrays.asList("a", "b", "c", "d"))
+                .doOnNext(System.out::println)
+                .transform(fillterAndMap);
+
+        transformFlux.subscribe(System.out::println);
+
+    }
 
 }
