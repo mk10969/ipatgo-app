73ff49197ee87fbad526c48d91786a47d41cba8e
mk10969 <mk10969@gmail.com>
2020-02-17 22:49:14 +0900

    ipatgoとjvlinkを１つにした

---
+++ uma-external-app/src/test/java/org/uma/external/ExternalApplicationTest.java
@@ -0,0 +1,15 @@
+package org.uma.external;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExternalApplicationTest {
+
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+
+}
